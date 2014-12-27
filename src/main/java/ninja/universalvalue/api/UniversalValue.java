cb550fae5decddc3244c63d5c75f20c4ad71855f
ViolentNinjaD <fiddlybilly@gmail.com>
2014-12-27 14:27:02 +0000

    Work on the basic universal value class :0

---
+++ src/main/java/ninja/universalvalue/api/UniversalValue.java
@@ -0,0 +1,46 @@
+package ninja.universalvalue.api;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+
+public class UniversalValue
+{
+    public UniversalValue(int value)
+    {
+
+    }
+
+    public static int getValue(Item item)
+    {
+        //TODO: Complete this method
+        return 1;
+    }
+
+    public static int getValue(Block block)
+    {
+        //|TODO: Complete this method
+        return 1;
+    }
+
+    public static int getValue(ItemStack itemStack)
+    {
+        //|TODO: Complete this method
+        return 1;
+    }
+
+    public static void setValue(Item item, int value)
+    {
+        //|TODO: Complete this method
+    }
+
+    public static void setValue(Block block, int value)
+    {
+        //|TODO: Complete this method
+    }
+
+    public static void setValue(ItemStack itemStack, int value)
+    {
+        //|TODO: Complete this method
+    }
+}
