bed9b4c6c00a15582b509cfd03f054fe1b97101d
ViolentNinjaD <fiddlybilly@gmail.com>
2014-12-27 09:45:16 +0000

    Mod loads in Minecraft :)

--- src/main/java/ninja/universalvalue/UVMod.java
+++ src/main/java/ninja/universalvalue/UVMod.java
@@ -1,5 +1,13 @@
 package ninja.universalvalue;
 
-public class UVMod 
+import cpw.mods.fml.common.Mod;
+
+import static ninja.universalvalue.UVMod.*;
+
+@Mod(modid = MOD_ID, name = MOD_NAME, version = MOD_VERSION)
+public class UVMod
 {
+    public static final String MOD_ID = "universalvalue";
+    public static final String MOD_NAME = "UniversalValue";
+    public static final String MOD_VERSION = "@VERSION@";
 }
